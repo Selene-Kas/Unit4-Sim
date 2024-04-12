fd53f23ceeaa96c8c34647c4d71a9a8181a1955c
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-04-11 21:55:51 -0700

    complete cart routes

---
+++ server/routes/cart.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const router = express.Router();
+const {  
+    fetchAllCarts,
+    fetchCart,
+    fetchCartProducts,
+    fetchUserCart,
+    createCartProduct,
+    deleteCartProduct
+} = require('../db');
+
+// get route for all carts
+router.get('/', async(req,res,next)=> {
+  try {
+    res.send(await fetchAllCarts());
+  } catch(ex) {
+    next(ex);
+  }
+});
+//get route for single cart ('/api/carts')
+router.get('/:cartId', async (req,res, next)=> {
+  try{
+    res.send(await fetchCart(req.params.cartId));
+  } catch(ex) {
+    next(ex);
+  }
+})
+//get cart by userId 
+router.get('/user/:userId', async(req,res, next)=> {
+  try{
+    res.send(await fetchUserCart(req.params.userId));
+  } catch(err) {
+    next(err);
+  }
+});
+
+//get route for cart products
+router.get('/:cartId/cart_products', async(req, res, next)=> {
+  try{
+    res.send(await fetchCartProducts(req.params.cartId));
+  } catch(err) {
+    next(err);
+  }
+});
+
+// POST route for creating a cartProduct. Adding a product to cart
+router.post('/:cartId/cart_products/:cartProductId', async(req, res, next)=> {
+  try {
+    res.status(201).send(await createCartProduct(req.params.cartId, req.params.cartProductId, req.body.qty));
+  } catch(err) {
+    next(err);
+  }
+});
+
+// delete cart_product
+router.delete('/:cartId/cart_products/:cartProductId', async(req, res, next)=> {
+  try{
+    await deleteCartProduct(req.params.cartId, req.params.cartProductId);
+    res.status(204);
+  } catch(err) {
+    next(err);
+  }
+})
+
+  
+
+module.exports = router;
